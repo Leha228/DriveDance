57c05b9731a88da8b4d0f272ec7284e437c87397
Leha228 <ryabin.01@mail.ru>
2020-12-07 20:37:59 -0800

    Create news, pricing page

---
+++ DriveDance/resources/views/layouts/app.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="app.css">
+    <title>DriveDance</title>
+</head>
+<body>
+    
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark" >
+        <div style="width: 70%; margin:auto; display:flex; justify-content:space-around">
+            <ul class="navbar-nav mr-auto" >
+                <li class="nav-item active">
+                    <a class="nav-link" href="{{ route('home') }}">DriveDance<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('news') }}">Новости</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Расписание занятий</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('pricing') }}">Цены</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Акции</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Вакансии</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Отзывы</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Контакты</a>
+                </li>
+            </ul>
+            <form class="form-inline mt-2 mt-md-0">
+                <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+            </div>
+        </div>
+    </nav>
+
+    <main role="main">
+        @yield('content')
+        <footer class="container mt-5 mb-5">
+            <p class="float-right"><a href="#">Back to top</a></p>
+            <p>© 2020 Company, Inc. · <a href="#">Privacy</a> · <a href="#">Terms</a></p>
+        </footer>
+    </main>
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
--- DriveDance/routes/web.php
+++ DriveDance/routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return view('home');})->name('home');
+Route::get('/news', function () { return view('news');})->name('news');
+Route::get('/pricing', function () { return view('pricing');})->name('pricing');
