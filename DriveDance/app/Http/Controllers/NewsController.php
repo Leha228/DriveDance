47240d275b0713ad62f20089b1124caff83b400f
Leha228 <ryabin.01@mail.ru>
2020-12-09 14:35:53 +0500

    Update

--- DriveDance/app/Http/Controllers/NewsController.php
+++ DriveDance/app/Http/Controllers/NewsController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\DB;
 class NewsController extends Controller
 {
     public function index() {
-        $posts = DB::table('posts')->get();
+        $posts = DB::table('posts')->get()->reverse();
         if (count($posts) == 0) 
             return view('news');
         return view('news', ['posts' => $posts]);
