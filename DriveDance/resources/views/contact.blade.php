2233b15a698d2c1a22b87a4ded4f7be15356d689
Leha228 <ryabin.01@mail.ru>
2020-12-12 11:13:10 +0500

    Day 4 new

--- DriveDance/resources/views/contact.blade.php
+++ DriveDance/resources/views/contact.blade.php
@@ -5,7 +5,7 @@
     <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
         <h1 class="display-4">Контакты</h1>
     </div>
-    <div style="position:relative;overflow:hidden;"><a href="https://yandex.ru/maps/213/moscow/?utm_medium=mapframe&utm_source=maps" style="color:#eee;font-size:12px;position:absolute;top:0px;">Москва</a><a href="https://yandex.ru/maps/profile?ll=37.622504%2C55.753215&utm_medium=mapframe&utm_source=maps&z=10" style="color:#eee;font-size:12px;position:absolute;top:14px;">Яндекс.Карты</a><iframe src="https://yandex.ru/map-widget/v1/-/CCQh4PWy0C" width="100%" height="480" frameborder="1" allowfullscreen="true" style="position:relative;"></iframe></div>
+    <div style="position:relative;overflow:hidden;"><a href="https://yandex.ru/maps/213/moscow/?utm_medium=mapframe&utm_source=maps" style="color:#eee;font-size:12px;position:absolute;top:0px;">Москва</a><a href="https://yandex.ru/maps/213/moscow/house/ulitsa_petrovka_38s1/Z04YcAZiQE0FQFtvfXt3eH9kbA==/?ll=37.612982%2C55.769378&utm_medium=mapframe&utm_source=maps&z=16" style="color:#eee;font-size:12px;position:absolute;top:14px;">Улица Петровка, 38с1 на карте Москвы, ближайшее метро Чеховская — Яндекс.Карты</a><iframe src="https://yandex.ru/map-widget/v1/-/CCUEVMtVgC" width="100%" height="480" frameborder="1" allowfullscreen="true" style="position:relative;"></iframe></div>
 </div>
 
 @endsection
